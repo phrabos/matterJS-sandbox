e8d5cd5ebfc7e269ec9a12b6730c3fde9033cc53
patrick hrabos <phrabos@gmail.com>
2021-06-08 23:25:20 -0400

    shapes play sounds when collision

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -26,8 +26,8 @@ const Scene = () => {
       element: sceneRef.current,
       engine: engine,
       options: {
-        width: 600,
-        height: 600,
+        width: 800,
+        height: 800,
         wireframes: false
       }
     });
@@ -38,8 +38,8 @@ const Scene = () => {
       // walls
       Bodies.rectangle(200, 0, 600, 50, { isStatic: true }),
       Bodies.rectangle(200, 600, 600, 50, { isStatic: true }),
-      Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
-      Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+      // Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
+      // Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
     ]);
 
     World.add(engine.world, [ballA, ballB]);
@@ -58,21 +58,30 @@ const Scene = () => {
   
       World.add(engine.world, mouseConstraint);
   
-      Matter.Events.on(mouseConstraint, "mousedown", function(event) {
-        World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-      });
+      // Matter.Events.on(mouseConstraint, "mousedown", function(event) {
+      //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
+      // });
   
       Matter.Runner.run(engine);
   
       Render.run(render);
+      const synth = new Tone.Synth().toDestination();
+      const synth2 = new Tone.Synth().toDestination();
+
       Matter.Events.on(engine, 'collisionStart', function(event) {
         if(event){
+          console.log('event', event)
           let a = event.source.pairs.list[0].bodyA.label
-          // let b = event.source.pairs.list[0].bodyB.label
-          const synth = new Tone.Synth().toDestination();
-          if(a)synth.triggerAttackRelease('C4', '4n');
-          // if(b)synth.triggerAttackRelease('F4', '4n');
-          console.log('a', a)
+          let b = event.source.pairs.list[1]?.bodyB.label
+          if(a === "Circle Body"){
+
+            synth.triggerAttackRelease('C4', '8n')
+             console.log('a', a)
+          }
+          if(b === "Circle Body"){
+            synth2.triggerAttackRelease('F4', '4n');
+            console.log('b', b)
+          }
         } 
         // let b = event.pairs[1] ? event.pairs[1] : null;
         // let b = event.pairs[1] ? event.pairs[1] : null
