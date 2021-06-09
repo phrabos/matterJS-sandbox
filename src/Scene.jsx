69d91d9ea69618e05aff7820e3762c6f519a6cdf
patrick hrabos <phrabos@gmail.com>
2021-06-09 11:13:40 -0400

    scene loads onMount, separate button to activate sounds within the world

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,18 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import Matter from "matter-js";
 import * as Tone from 'tone';
 
 const Scene = () => {
   // const [scene, setScene] = useState();
   const sceneRef = useRef(null);
+  const engineRef = useRef(null);
 
-
-
-
-  const handleClick = async () => {
-    console.log('clicked');
-    // const synth = new Tone.Synth().toDestination();
-    await Tone.start();
+  useEffect(() => {
     let Engine = Matter.Engine;
     let Render = Matter.Render;
     let World = Matter.World;
@@ -20,11 +15,12 @@ const Scene = () => {
     let Mouse = Matter.Mouse;
     let MouseConstraint = Matter.MouseConstraint;
 
-    let engine = Engine.create({});
+    engineRef.current = Engine.create({});
+    engineRef.current.gravity.y = 1.3;
 
     let render = Render.create({
       element: sceneRef.current,
-      engine: engine,
+      engine: engineRef.current,
       options: {
         width: 800,
         height: 800,
@@ -32,21 +28,21 @@ const Scene = () => {
       }
     });
 
-    let ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
-    let ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
-    World.add(engine.world, [
+    let ballA = Bodies.circle(210, 100, 10, { restitution: 0.5 });
+    let ballB = Bodies.circle(110, 50, 10, { restitution: 0.5 });
+    World.add(engineRef.current.world, [
       // walls
       Bodies.rectangle(200, 0, 600, 50, { isStatic: true }),
       Bodies.rectangle(200, 600, 600, 50, { isStatic: true }),
-      // Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
-      // Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+      Bodies.rectangle(260, 300, 50, 600, { isStatic: true }),
+      Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
     ]);
 
-    World.add(engine.world, [ballA, ballB]);
+    World.add(engineRef.current.world, [ballA, ballB]);
 
         // add mouse control
         let mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
+        mouseConstraint = MouseConstraint.create(engineRef.current, {
           mouse: mouse,
           constraint: {
             stiffness: 0.2,
@@ -56,39 +52,47 @@ const Scene = () => {
           }
         });
   
-      World.add(engine.world, mouseConstraint);
+      World.add(engineRef.current.world, mouseConstraint);
   
       // Matter.Events.on(mouseConstraint, "mousedown", function(event) {
       //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
       // });
   
-      Matter.Runner.run(engine);
+      Matter.Runner.run(engineRef.current);
   
       Render.run(render);
-      const synth = new Tone.Synth().toDestination();
+      
+
+  }, []);
+
+  const handleClick = async () => {
+    console.log('clicked');
+    // const synth = new Tone.Synth().toDestination();
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
       const synth2 = new Tone.Synth().toDestination();
 
-      Matter.Events.on(engine, 'collisionStart', function(event) {
+      Matter.Events.on(engineRef.current, 'collisionStart', function(event) {
         if(event){
           console.log('event', event)
           let a = event.source.pairs.list[0].bodyA.label
-          let b = event.source.pairs.list[1]?.bodyB.label
-          if(a === "Circle Body"){
+          let b = event.source.pairs.list[0].bodyB.label
+          if(a === "Circle Body" && b === "Circle Body"){
 
             synth.triggerAttackRelease('C4', '8n')
              console.log('a', a)
           }
-          if(b === "Circle Body"){
-            synth2.triggerAttackRelease('F4', '4n');
-            console.log('b', b)
-          }
+          // if(b === "Circle Body"){
+          //   synth2.triggerAttackRelease('F4', '4n');
+          //   console.log('b', b)
+          // }
         } 
         // let b = event.pairs[1] ? event.pairs[1] : null;
         // let b = event.pairs[1] ? event.pairs[1] : null
       
         // check bodies, do whatever...
       });
-
+    
   }
 
   return( 
